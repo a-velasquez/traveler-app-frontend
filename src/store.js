62929736b461b7c22083c2394615b6c6eca15314
Alex Velazquez <avelasquez075@gmail.com>
2021-04-27 18:44:50 -0700

    import loginForm and currentUser add combineReducers

--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import currentUser from "./reducers/currentUser.js";
+import loginForm from "./reducers/loginForm.js";
+
+const reducer = combineReducers({
+  currentUser,
+  loginForm
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
